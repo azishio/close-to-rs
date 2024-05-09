4b2c44c28e486add7e86ce352863e665cc86b864
azishio <azishio3141@gmail.com>
2024-05-10 06:34:12 +0900

    💥  boolだけでなく差分も返すように変更 より広い型に適用できるように変更

--- src/lib.rs
+++ src/lib.rs
@@ -9,36 +9,25 @@ use num::Float;
 /// ```
 /// use close_to::close_to;
 ///
-/// assert!(close_to(1.0, 1.0001, 3));
+/// let (is_close, expected_diff, received_diff) = close_to(1.0, 1.0001, 3);
+/// assert!(is_close);
 ///```
 /// ```should_panic
 /// use close_to::close_to;
 ///
-/// assert!(close_to(1.0, 1.0001, 4));
+/// let (is_close, expected_diff, received_diff) = close_to(1.0, 1.0001, 4);
+/// assert!(is_close);
 /// ```
-///
-/// この関数は以下のようにして、異なる型の小数を比較することもできる。
-/// このとき、右辺の型は、左辺の型へのIntoトレイトを実装している必要がある。
-///
-/// # Examples
-///
 /// ```
-/// use close_to::close_to;
-///
-/// assert!(close_to(1.0_f64, 1.0001_f32, 3));
-/// assert!(!close_to(1.0_f64, 1_u8, 4));
-/// ```
-///
 pub fn close_to<T: Float, U: Into<T>>(left: T, right: U, precision: i32) -> (bool, T, T)
 {
     let expected_diff = T::from(10).unwrap().powi(-precision) / T::from(2).unwrap();
 
     let received_diff = (left - right.into()).abs();
 
-    received_diff <= expected_diff
+    (received_diff <= expected_diff, expected_diff, received_diff)
 }
 
-pub fn assert_close_to<T: Float + Display, U: Float + Display + Into<T>>(left: T, right: U, precision: i32)
 /// ２つの小数が指定した精度で等しいことを保証する。
 /// パニックになると、この関数は小数の値をデバッグ表現とともに出力する。
 ///
@@ -60,27 +49,18 @@ pub fn assert_close_to<T: Float + Display, U: Float + Display + Into<T>>(left: T
 /// // received_diff: 0.00009999999999998899
 /// // expected_diff: 0.00005
 /// ```
-///
-/// この関数は以下のようにして、異なる型の小数を比較することもできる。
-/// このとき、右辺の型は、左辺の型へのIntoトレイトを実装している必要がある。
-///
-/// # Examples
-///
-/// ```
-/// use close_to::assert_close_to;
-///
-/// assert_close_to(1.0_f64, 1.0001_f32, 3);
 /// ```
 pub fn assert_close_to<T: Float + Display + Copy, U: Display + Into<T> + Copy>(left: T, right: U, precision: i32)
 {
-    if !close_to(left, right, precision)
+    let (is_close, expected_diff, received_diff) = close_to(left, right, precision);
+
+    if !is_close
     {
-        panic!("assertion 'left ≈ right` failed\n left: {}\nright: {}\nreceived_diff: {}\nexpected_diff: {}", left, right, (left - right.into()).abs(), T::from(10).unwrap().powi(-precision) / T::from(2).unwrap());
+        panic!("assertion 'left ≈ right` failed\n left: {}\nright: {}\nreceived_diff: {}\nexpected_diff: {}", left, right, received_diff, expected_diff);
     }
 }
 
 
-pub fn far_from<T: Float, U: Float + Into<T>>(left: T, right: U, precision: i32) -> bool
 /// 2つの小数が指定した精度で等しくないかどうかを判定する。
 ///
 /// # Examples
@@ -88,18 +68,22 @@ pub fn far_from<T: Float, U: Float + Into<T>>(left: T, right: U, precision: i32)
 /// ```
 /// use close_to::far_from;
 ///
-/// assert!(far_from(1.0, 1.0001, 4));
+/// let (is_far, expected_diff, received_diff) = far_from(1.0, 1.0001, 4);
+/// assert!(is_far);
 /// ```
 /// ```should_panic
 /// use close_to::far_from;
 ///
-/// assert!(far_from(1.0, 1.0001, 3));
+/// let (is_far, expected_diff, received_diff) = far_from(1.0, 1.0001, 3);
+/// assert!(is_far);
 /// ```
+pub fn far_from<T: Float, U: Into<T>>(left: T, right: U, precision: i32) -> (bool, T, T)
 {
-    !close_to(left, right, precision)
+    let (is_close, expected_diff, received_diff) = close_to(left, right, precision);
+
+    (!is_close, expected_diff, received_diff)
 }
 
-pub fn assert_far_from<T: Float + Display, U: Float + Display + Into<T>>(left: T, right: U, precision: i32)
 
 /// 2つの小数が指定した精度で等しくないことを保証する。
 /// パニックになると、この関数は小数の値をデバッグ表現とともに出力する。
@@ -122,47 +106,78 @@ pub fn assert_far_from<T: Float + Display, U: Float + Display + Into<T>>(left: T
 /// // received_diff: 0.00009999999999998899
 /// // expected_diff: 0.0005
 /// ```
+pub fn assert_far_from<T: Float + Display + Copy, U: Display + Into<T> + Copy>(left: T, right: U, precision: i32)
 {
-    if !far_from(left, right, precision)
+    let (is_far, expected_diff, received_diff) = far_from(left, right, precision);
+
+    if !is_far
     {
-        panic!("assertion 'left != right` failed\n left: {}\nright: {}\nreceived_diff: {}\nexpected_diff: {}", left, right, (left - right.into()).abs(), T::from(10).unwrap().powi(-precision) / T::from(2).unwrap());
+        panic!("assertion 'left != right` failed\n left: {}\nright: {}\nreceived_diff: {}\nexpected_diff: {}", left, right, received_diff, expected_diff);
     }
 }
 
-pub trait CloseTo
+/// 2つの小数が指定した精度で等しいかどうかを判定するトレイト。
+/// デフォルトでfloat型に実装されている。
+///
+/// # Examples
+///
+/// ```
+/// use close_to::CloseTo;
+///
+/// let (is_close, ..) = 1.0.close_to(1.0001, 3);
+/// assert!(is_close);
+///
+/// assert!(1.0.far_from(1.0001, 4).0);
+/// ```
+pub trait CloseTo<T, U: Into<T>>
 {
-    fn close_to(&self, other: Self, precision: i32) -> bool;
-    fn far_from(&self, other: Self, precision: i32) -> bool;
+    /// 2つの小数が指定した精度で等しいかどうかを判定する。
+    fn close_to(&self, other: U, precision: i32) -> (bool, T, T);
+    /// 2つの小数が指定した精度で等しくないかどうかを判定する。
+    fn far_from(&self, other: U, precision: i32) -> (bool, T, T);
 }
 
-pub trait AssertCloseTo
+/// 2つの小数が指定した精度で等しいことを保証するトレイト。
+/// デフォルトでfloat型に実装されている。
+///
+/// # Examples
+///
+/// ```
+/// use close_to::{assert_far_from, AssertCloseTo};
+///
+/// 1.0.assert_close_to(1.0001, 3);
+/// 1.0.assert_far_from(1.0001, 4);
+/// ```
+pub trait AssertCloseTo<T>
 {
-    fn assert_close_to(&self, other: Self, precision: i32);
-    fn assert_far_from(&self, other: Self, precision: i32);
+    /// 2つの小数が指定した精度で等しいことを保証する。
+    fn assert_close_to(&self, other: T, precision: i32);
+    /// 2つの小数が指定した精度で等しくないことを保証する。
+    fn assert_far_from(&self, other: T, precision: i32);
 }
 
 
-impl<T: Float> CloseTo for T
+impl<T: Float, U: Into<T>> CloseTo<T, U> for T
 {
-    fn close_to(&self, other: T, precision: i32) -> bool
+    fn close_to(&self, other: U, precision: i32) -> (bool, T, T)
     {
         close_to(*self, other, precision)
     }
 
-    fn far_from(&self, other: T, precision: i32) -> bool
+    fn far_from(&self, other: U, precision: i32) -> (bool, T, T)
     {
         far_from(*self, other, precision)
     }
 }
 
-impl<T: Float + Display> AssertCloseTo for T
+impl<T: Float + Display, U: Into<T> + Display + Copy> AssertCloseTo<U> for T
 {
-    fn assert_close_to(&self, other: T, precision: i32)
+    fn assert_close_to(&self, other: U, precision: i32)
     {
         assert_close_to(*self, other, precision)
     }
 
-    fn assert_far_from(&self, other: T, precision: i32)
+    fn assert_far_from(&self, other: U, precision: i32)
     {
         assert_far_from(*self, other, precision)
     }
@@ -176,8 +191,11 @@ mod test
     #[test]
     fn test_close_to()
     {
-        assert!(close_to(1.0, 1.0001, 3));
-        assert!(!close_to(1.0, 1.0001, 4));
+        let (is_close, ..) = close_to(1.0, 1.0001, 3);
+        assert!(is_close);
+
+        let (is_close, ..) = close_to(1.0, 1.0001, 4);
+        assert!(!is_close);
     }
 
     #[test]
@@ -204,8 +222,11 @@ mod test
     #[test]
     fn test_trait()
     {
-        assert!(1.0.close_to(1.0001, 3));
-        assert!(!1.0.close_to(1.0001, 4));
+        let (is_close, ..) = 1.0.close_to(1.0001, 3);
+        assert!(is_close);
+
+        let (is_close, ..) = 1.0.close_to(1.0001, 4);
+        assert!(!is_close);
     }
 
     #[test]
